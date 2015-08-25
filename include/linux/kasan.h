f104c1db7850ca525da7cfa5cf70a12ccaf34f9e
Alexander Potapenko <glider@google.com>
2015-08-25 16:47:23 +0200

    Fix compilation with CONFIG_KASAN disabled

--- include/linux/kasan.h
+++ include/linux/kasan.h
@@ -90,6 +90,13 @@ static inline void kasan_disable_local(void) {}
 static inline void kasan_alloc_pages(struct page *page, unsigned int order) {}
 static inline void kasan_free_pages(struct page *page, unsigned int order) {}
 
+static inline void kasan_cache_create(struct kmem_cache *cache,
+				      cache_size_t *size,
+				      unsigned long *flags) {};
+static inline void kasan_cache_shrink(struct kmem_cache *cache) {};
+static inline void kasan_cache_destroy(struct kmem_cache *cache) {};
+
+
 static inline void kasan_poison_slab(struct page *page) {}
 static inline void kasan_unpoison_object_data(struct kmem_cache *cache,
 					void *object) {}
@@ -97,13 +104,13 @@ static inline void kasan_poison_object_data(struct kmem_cache *cache,
 					void *object) {}
 
 
-static inline void kasan_kmalloc_large(void *ptr, size_t size) {}
+static inline void kasan_kmalloc_large(void *ptr, size_t size, gfp_t flags) {}
 static inline void kasan_kfree_large(const void *ptr) {}
 static inline void kasan_kmalloc(struct kmem_cache *s, const void *object,
-				size_t size) {}
-static inline void kasan_krealloc(const void *object, size_t new_size) {}
+				size_t size, gfp_t flags) {}
+static inline void kasan_krealloc(const void *object, size_t new_size, gfp_t flags) {}
 
-static inline void kasan_slab_alloc(struct kmem_cache *s, void *object) {}
+static inline void kasan_slab_alloc(struct kmem_cache *s, void *object, gfp_t flags) {}
 static inline void kasan_slab_free(struct kmem_cache *s, void *object) {}
 
 #define MODULE_ALIGN 1
